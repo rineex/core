8588e14dcd3e5130c7bda78f6eb95beb3084459f
Hossein Pourdavar <h.pourdavar@gmail.com>
2025-12-18 09:22:04 +0330

    chore: simplify build script in package.json and update tsup configuration for better clarity

--- packages/ddd/tsup.config.ts
+++ packages/ddd/tsup.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
+export default defineConfig(() => ({
+  // eslint-disable-next-line n/no-process-env
+  minify: process.env.CI === 'true',
+  tsconfig: 'tsconfig.build.json',
   entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
   splitting: false,
   sourcemap: true,
   clean: true,
-});
+  dts: true,
+}));
