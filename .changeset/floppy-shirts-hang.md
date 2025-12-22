7f14ba69676736a3b1fad984bdc7fcce3ead6640
Hossein Pourdavar <h.pourdavar@gmail.com>
2025-12-22 15:59:07 +0330

    feat(value-objects): export id value object from domain value-objects

---
+++ .changeset/floppy-shirts-hang.md
@@ -0,0 +1,5 @@
+---
+'@rineex/ddd': patch
+---
+
+export id value object from domain value-objects
--- packages/ddd/src/index.ts
+++ packages/ddd/src/index.ts
@@ -8,5 +8,6 @@ export * from './domain/errors/entity-validation.error';
 export * from './domain/errors/invalid-vo.error';
 export * from './domain/events';
 export * from './domain/value-objects/aggregate-id.vo';
+export * from './domain/value-objects/id.vo';
 export * from './gateway/constants/http-code';
 export * from './utils';
